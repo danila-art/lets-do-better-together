62eeb9fde2fb28008ea4762e5b6dda09bf3cfe3c
danila-art <danila.sivov.96@mail.ru>
2021-04-07 14:29:15 +0300

    add image to bd

--- php/add-aplication.php
+++ php/add-aplication.php
@@ -1,11 +1,19 @@
 <?php
+$login_user = $_COOKIE['loginUser'];
+$id_user = '';
+require_once 'connection.php';
 $aplication__name = $_POST['aplication__name'];
 $aplication__description = $_POST['aplication__description'];
 $aplication__category = $_POST['aplication__category'];
-$aplication__img_before = $_FILES['aplication__img_before'];
-echo $aplication__name. "<br>";
-echo $aplication__description. "<br>";
-echo $aplication__category. "<br>";
-echo $aplication__img_before. "<br>";
-// Доделать
+$aplication__img_before_name = $_FILES['aplication__img_before']['name'];
+$aplication__img_before_tmp = addslashes(file_get_contents($_FILES['aplication__img_before']['tmp_name']));
+$date = date("Y:m:d");
+$linkToDataBase = mysqli_connect($host, $user, $password, $database);
+$thisUser = $linkToDataBase->query("SELECT * FROM `user` WHERE `login` = '$login_user'");
+while($result = mysqli_fetch_assoc($thisUser)){
+$id_user = $result['id'];
+}
+echo $id_user;
+$linkToDataBase->query("INSERT INTO `aplication`(`name`, `description`, `category`, `date`, `img-before-name`, `img-before-tmp`, `id_user`) VALUES ('$aplication__name', '$aplication__description', '$aplication__category', '$date', '$aplication__img_before_name','$aplication__img_before_tmp', '$id_user')");
+$linkToDataBase->close();
 ?>
